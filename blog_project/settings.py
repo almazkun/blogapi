00b9a91d4e46772f6ab4e4d6fd759b29a6636a82
Almaz Kunpeissov <almaz.kun@gmail.com>
2019-02-18 11:07:54 +0600

    User Authentication

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -37,10 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 
     # 3rd party
     'rest_framework',
     'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'rest_auth',
+    'rest_auth.registration',
 
     # Local
     'posts.apps.PostsConfig',
@@ -135,3 +141,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication'
     ],
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+SITE_ID = 1
